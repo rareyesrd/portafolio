4a50e2384125d01afe9ef52013fca1cbcece0b01
Rafael Alexander Reyes Perez <rafaelalexanderr5@gmail.com>
2020-02-24 18:54:41 -0400

    ADD - router transition and integrate sass

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <div id="app">
-    <div class="left">
-      <Dashboard />
+  <VueScrollProgress>
+    <div id="app">
+      <div class="left">
+        <Dashboard />
+      </div>
+      <div class="right">
+        <transition 
+        name="fade"
+        mode="out-in">
+        <router-view></router-view>
+        </transition>
+      </div>
     </div>
-    <div class="right">
-      <router-view>
-      </router-view>
-    </div>
-  </div>
+  </VueScrollProgress>
 </template>
 
 <script>
-import Dashboard from './components/Dashboard.vue';
+import Dashboard from "./components/Dashboard.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Dashboard, 
-  },
-}
+    Dashboard
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,12 +33,31 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.right{
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+
+.right {
   width: 100%;
-} 
-@media(min-width:768px){
-  .right{
-    width: 80%;
+  @media (min-width: 768px) {
+      width: 80%;
   }
+}
+#progress-container-el {
+  /* background */
+  background-color: #44cf28 !important;
+}
+
+#progress-el {
+  /* progress bar */
+  background-color: red !important;
 }
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <h1>Projects Working</h1>
+      
   </div>
 </template>
 
